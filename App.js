e737a3ef774b1b31353329e9f599554486770d59
Miika Mikkonen <miika.mikkonen5@gmail.com>
2022-11-21 18:47:52 +0200

    Started 10.15

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import { NativeRouter } from 'react-router-native';
 import Constants from 'expo-constants'
 import Main from './src/components/Main';
 import createApolloClient from './src/utils/apolloClient';
+import AuthStorage from './src/utils/authStorage';
 
-const apolloClient = createApolloClient();
+const authStorage = new AuthStorage();
+const apolloClient = createApolloClient(authStorage);
 
 export default function App() {
   console.dir(Constants.manifest);
