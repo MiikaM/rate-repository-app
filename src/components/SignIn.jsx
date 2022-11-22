b2a31d1be2b61a410afc22091948a74334ee7b6d
Miika Mikkonen <miika.mikkonen5@gmail.com>
2022-11-22 15:41:57 +0200

    fix: Finished 10.15 (for real)

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -5,6 +5,7 @@ import FormikTextInput from './FormikTextInput';
 import Text from './Text';
 import * as yup from "yup"
 import useSignIn from './hooks/useSignIn';
+import { useNavigate } from 'react-router-native';
 
 const styles = StyleSheet.create({
     button: {
@@ -23,12 +24,14 @@ const validationSchema = yup.object().shape({
 
 const SignIn = () => {
     const [signIn] = useSignIn();
+    let navigate = useNavigate();
 
     const onSubmit = async (values) => {
         const { username, password } = values;
         try {
             const { data } = await signIn({username, password});
             console.log(`Congratulations you logged in as user with accesstoken: ${data.authenticate.accessToken}`);
+            navigate("../", {replace: true})
         } catch (error) {
             console.error({ error });
         }
